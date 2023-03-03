44e3e42351be7bf3f3556eb04acbc238f9484ccd
JokerHuu1 <1361116277@qq.com>
2023-03-03 17:56:36 +0800

    Update jrskan2.js
    Signed-off-by: JokerHuu1 <1361116277@qq.com>

--- tvbox/jrskan2.js
+++ tvbox/jrskan2.js
@@ -1,3 +1,14 @@
+// 道长 drpy仓库 https://gitcode.net/qq_32394351/dr_py
+// 道长 drpy安卓本地搭建说明 https://gitcode.net/qq_32394351/dr_py/-/blob/master/%E5%AE%89%E5%8D%93%E6%9C%AC%E5%9C%B0%E6%90%AD%E5%BB%BA%E8%AF%B4%E6%98%8E.md
+// 道长 drpy写源 模板规则说明 https://gitcode.net/qq_32394351/dr_py#%E6%A8%A1%E6%9D%BF%E8%A7%84%E5%88%99%E8%AF%B4%E6%98%8E
+// 道长 drpy写源 套模模版 https://gitcode.net/qq_32394351/dr_py/-/raw/master/js/%E6%A8%A1%E6%9D%BF.js
+// 道长 drpy写源 相关视频教程 https://www.youtube.com/watch?v=AK7cN-fcwm4
+// 道长 drpy写源 写源教学视频 https://t.me/fongmi_offical/54080/63553
+// 海阔下载 https://haikuo.lanzoui.com/u/GoldRiver
+// 影视TV 官方TG Drpy群 https://t.me/fongmi_offical/63689
+// 影视TV 官方TG 下载 https://t.me/fongmi_release
+
+
 var rule = {
     title:'JRSKAN直播',
     host:'http://www.jrskan.com/?lan=1',
@@ -7,7 +18,7 @@ var rule = {
     searchUrl:'',
     searchable:0,
     quickSearch:0,
-    class_name:'全部赛事',
+    class_name:'全部',
     class_url:'/',
     //class_url:'?live',
     headers:{
@@ -20,7 +31,7 @@ var rule = {
     double:false,
     推荐:'*',
     // 一级:'.loc_match:eq(2) ul;li:gt(1):lt(4)&&Text;img&&src;li:lt(2)&&Text;a:eq(1)&&href',//play.sportsteam333.com
-    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.d-touch');tabs.forEach(function(it){var pz=pdfh(it,'.name:eq(1)&&Text');var ps=pdfh(it,'.name:eq(0)&&Text');var pk=pdfh(it,'.name:eq(2)&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.lab_time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+' '+ps,title:pz+'vs'+pk,pic_url:img,url:url})});setResult(items);",
+    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.d-touch');tabs.forEach(function(it){var pz=pdfh(it,'.name:eq(1)&&Text');var ps=pdfh(it,'.name:eq(0)&&Text');var pk=pdfh(it,'.name:eq(2)&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.lab_time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'🏆'+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
     二级:{
 		"title":".sub_list li:lt(2)&&Text;.sub_list li:eq(0)&&Text",
 		"img":"img&&src",
@@ -31,3 +42,4 @@ var rule = {
 	},
     搜索:'',
 }
+
